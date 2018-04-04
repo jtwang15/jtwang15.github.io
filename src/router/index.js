3e14607ab7ce2506ec07567c52abd4e1b948d105
jtwang <angellovecan@126.com>
2018-04-04 09:08:49 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from "@/components/Home"
+import JiaNing from '@/components/JiaNing'
 Vue.use(Router)
 
 export default new Router({
   mode: 'hash',
-  routes: [{
-    path: '/',
-    name: 'home',
-    component: Home
-  }, ]
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/jianing',
+      name: 'jianing',
+      component: JiaNing
+    },
+  ]
 })
